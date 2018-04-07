74f4bf9704532413461eb7c0ca55130b9a846f54
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-04-07 11:52:30 -0700

    Improve Merge functionality

--- tile/tile.go
+++ tile/tile.go
@@ -1,27 +1,50 @@
 package tile
 
-import "errors"
+import (
+	"fmt"
+)
 
-type Tile struct {
-	value uint8
+type (
+	Value uint8
+
+	Tile struct {
+		value Value
+	}
+
+	IncompatibleTilesError struct {
+		t1 Tile
+		t2 Tile
+	}
+)
+
+func (tile Tile) Value() (value Value) {
+	return tile.value
 }
 
-func (tile Tile) Value() (value uint8) {
-	return 0
-}
-
-func (tile Tile) SetValue(value uint8) {
+func (tile *Tile) SetValue(value Value) {
 	tile.value = value
 }
 
 func (tile Tile) IsEmpty() bool {
-	return true
+	return tile.value == 0
 }
 
 func Merge(t1 Tile, t2 Tile) (tile Tile, err error) {
+	if t1.value < 3 && (t1.value == t2.value) {
+		return tile, IncompatibleTilesError{t1, t2}
+	}
+
+	if t1.value+t2.value == 3 {
+		return Tile{3}, err
+	}
+
 	if t1.value != t2.value {
-		return tile, errors.New("incompatible tile values")
+		return tile, IncompatibleTilesError{t1, t2}
 	}
 
 	return Tile{t1.value + t2.value}, err
+}
+
+func (e IncompatibleTilesError) Error() (err string) {
+	return fmt.Sprintf("Incompatible tile values: %v and %v", e.t1.value, e.t2.value)
 }
--- tile/tile_test.go
+++ tile/tile_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
+func TestTile_Value(t *testing.T) {
+	tile := Tile{0}
+
+	if tile.Value() != tile.value {
+		t.Errorf("Unexpected value: wanted %v, but got %v instead", tile.value, tile.Value())
+	}
+}
+
+func TestTile_SetValue(t *testing.T) {
+	var tile Tile
+	var value Value
+	tile.SetValue(value)
+
+	if tile.value != value {
+		t.Errorf("Unexpected value: should be %v, but got %v instead", value, tile.value)
+	}
+}
+
 func TestTile_IsEmpty(t *testing.T) {
 	var tile Tile
 
@@ -16,33 +34,61 @@ func TestTile_IsEmpty(t *testing.T) {
 	}
 }
 
-func TestTile_SetValue(t *testing.T) {
-	var tile Tile
-	var value uint8 = 0
-	tile.SetValue(value)
-
-	if tile.value != value {
-		t.Errorf("Unexpected value: should be %v, but got %v instead", value, tile.value)
-	}
-}
-
-func TestTile_Value(t *testing.T) {
-	tile := Tile{0}
-
-	if tile.Value() != tile.value {
-		t.Errorf("Unexpected value: wanted %v, but got %v instead", tile.value, tile.Value())
-	}
-}
-
 func TestMerge(t *testing.T) {
-	var t1, t2 Tile
+	t1 := Tile{3}
+	t2 := Tile{3}
 
 	tile, err := Merge(t1, t2)
 	if err != nil {
 		t.Error("Tiles of equal value were not merged")
 	}
 
-	if tile.value != t1.value + t2.value {
-		t.Errorf("Unepexted merge value: wanted %v, but got %v", t1.value + t2.value, tile.value)
+	if tile.value != t1.value+t2.value {
+		t.Errorf("Unepexted merge value: wanted %v, but got %v", t1.value+t2.value, tile.value)
+	}
+
+	t1.SetValue(6)
+
+	tile, err = Merge(t1, t2)
+	if err == nil {
+		t.Error("Tiles of unequal value cannot be merged")
+	}
+
+	t1.SetValue(1)
+	t2.SetValue(2)
+
+	tile, err = Merge(t1, t2)
+	if err != nil {
+		t.Error("Tile with value 1 not merged with tile of value 2")
+	}
+
+	if tile.value != 3 {
+		t.Errorf("Unepexted merge value: wanted %v, but got %v", 3, tile.value)
+	}
+
+	t1.SetValue(1)
+	t2.SetValue(1)
+
+	tile, err = Merge(t1, t2)
+	if err == nil {
+		t.Error("Tiles of value 1 can only be merged with tiles of value 2")
+	}
+
+	t1.SetValue(2)
+	t2.SetValue(2)
+
+	tile, err = Merge(t1, t2)
+	if err == nil {
+		t.Error("Tiles of value 1 can only be merged with tiles of value 2")
+	}
+}
+
+func TestIncompatibleTilesError_Error(t *testing.T) {
+	t1 := Tile{3}
+	t2 := Tile{3}
+	err := IncompatibleTilesError{t1, t2}.Error()
+
+	if err == "" {
+		t.Error("Error does not display incompatible values")
 	}
 }
