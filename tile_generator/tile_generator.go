892204a3b111ce5fce353f6097a03fa1667ad1fd
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-04-07 15:05:46 -0700

    Add tile generation functionality

--- tile_generator/tile_generator.go
+++ tile_generator/tile_generator.go
@@ -1,1 +1,28 @@
 package tile_generator
+
+import (
+	. "Threes/tile"
+)
+
+type (
+	SimpleTileGenerator struct{}
+	BonusTileGenerator  struct {
+		maxValue Value
+	}
+)
+
+func (SimpleTileGenerator) Generate() (tile Tile) {
+	tile.SetValue(1)
+
+	return
+}
+
+func (generator *BonusTileGenerator) SetMaxValue(maxTileValue Value) {
+	generator.maxValue = maxTileValue / 8
+}
+
+func (generator BonusTileGenerator) Generate() (tile Tile) {
+	tile.SetValue(generator.maxValue)
+
+	return
+}
--- tile_generator/tile_generator_test.go
+++ tile_generator/tile_generator_test.go
@@ -1,1 +1,40 @@
 package tile_generator
+
+import (
+	. "Threes/tile"
+	"testing"
+)
+
+func TestSimpleTileGenerator_Generate(t *testing.T) {
+	generator := SimpleTileGenerator{}
+	tile := generator.Generate()
+
+	if tile.IsEmpty() {
+		t.Error("Generated tiles cannot be empty")
+	}
+}
+
+func TestBonusTileGenerator_SetMaxValue(t *testing.T) {
+	var maxTileValue Value = 48
+
+	var generator BonusTileGenerator
+	generator.SetMaxValue(maxTileValue)
+
+	if generator.maxValue != maxTileValue/8 {
+		t.Error("Max value should be 1/8 of the maximum tile value")
+	}
+}
+
+func TestBonusTileGenerator_Generate(t *testing.T) {
+	var generator BonusTileGenerator
+	generator.SetMaxValue(48)
+	tile := generator.Generate()
+
+	if tile.IsEmpty() {
+		t.Error("Generated tiles cannot be empty")
+	}
+
+	if tile.Value() > generator.maxValue {
+		t.Errorf("Tile value is %v, but it cannot exceed %v", tile.Value(), generator.maxValue)
+	}
+}
