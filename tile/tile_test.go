90b0d3342225f76450aae5cd1e3d7088d19f5301
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-04-06 20:35:17 -0700

    Test tile's Value method

--- tile/tile_test.go
+++ tile/tile_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCreateTile(t *testing.T) {
+func TestTile_IsEmpty(t *testing.T) {
 	var tile Tile
 
 	if !tile.IsEmpty() {
@@ -16,12 +16,20 @@ func TestCreateTile(t *testing.T) {
 	}
 }
 
-func TestSetTileValue(t *testing.T) {
+func TestTile_SetValue(t *testing.T) {
 	var tile Tile
 	var value uint8 = 0
 	tile.SetValue(value)
 
 	if tile.value != value {
 		t.Errorf("Unexpected value: should be %v, but got %v instead", tile.value, value)
+	}
+}
+
+func TestTile_Value(t *testing.T) {
+	tile := Tile{0}
+
+	if tile.Value() != tile.value {
+		t.Errorf("Unexpected value: should be %v, but got %v instead", tile.Value(), tile.value)
 	}
 }
