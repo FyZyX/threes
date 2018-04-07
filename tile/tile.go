e90a520822294134539570223930e618763b6697
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-04-06 20:22:33 -0700

    Add functionality to set tile value

--- tile/tile.go
+++ tile/tile.go
@@ -8,6 +8,10 @@ func (tile Tile) Value() (value uint8) {
 	return 0
 }
 
+func (tile Tile) SetValue(value uint8) {
+	tile.value = value
+}
+
 func (tile Tile) IsEmpty() bool {
 	return true
 }
--- tile/tile_test.go
+++ tile/tile_test.go
@@ -15,3 +15,13 @@ func TestCreateTile(t *testing.T) {
 		t.Error("Empty tiles should have value 0")
 	}
 }
+
+func TestSetTileValue(t *testing.T) {
+	var tile Tile
+	var value uint8 = 0
+	tile.SetValue(value)
+
+	if tile.value != value {
+		t.Errorf("Unexpected value: should be %v, but got %v instead", tile.value, value)
+	}
+}
