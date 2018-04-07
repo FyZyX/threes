f8a05172b90a85cfd860155f9a865f7b276e4752
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-04-07 11:52:54 -0700

    Create functionality to add tiles to the board

--- board/board.go
+++ board/board.go
@@ -1,1 +1,27 @@
 package board
+
+import (
+	. "Threes/tile"
+)
+
+type (
+	Board [][]Tile
+
+	Index struct {
+		Row    uint8
+		Column uint8
+	}
+)
+
+func NewBoard() Board {
+	board := make([][]Tile, 4, 4)
+	for row := range board {
+		board[row] = make([]Tile, 4, 4)
+	}
+
+	return board
+}
+
+func (board Board) AddTile(tile Tile, index Index) {
+	board[index.Row][index.Column] = tile
+}
--- board/board_test.go
+++ board/board_test.go
@@ -1,1 +1,34 @@
 package board
+
+import (
+	. "Threes/tile"
+	"testing"
+)
+
+func TestNewBoard(t *testing.T) {
+	board := NewBoard()
+
+	if len(board) != 4 {
+		t.Error("Board should have 4 rows")
+	}
+
+	for _, row := range board {
+		if len(row) != 4 {
+			t.Error("Row should have 4 entires")
+		}
+	}
+}
+
+func TestBoard_AddTile(t *testing.T) {
+	var tile Tile
+	tile.SetValue(3)
+
+	board := NewBoard()
+	index := Index{0, 0}
+
+	board.AddTile(tile, index)
+
+	if board[index.Row][index.Column].IsEmpty() {
+		t.Error("Tile should not be empty")
+	}
+}
