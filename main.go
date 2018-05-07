c08a49b4ff948c7f4b7127c0584f91a43e21da8c
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-05-06 22:00:57 -0700

    Refactor to simplify testing

--- main.go
+++ main.go
@@ -6,22 +6,29 @@ import (
 	"math/rand"
 )
 
+var numGames int
+var cumScore float64
+
 func main() {
-	rand.Seed(459)
-	var numGames int
-	var cumScore float64
+	rand.Seed(42)
 	for i := 0; i < 10000; i++ {
-		game := NewGame()
-		var score float64
-		for !game.IsOver {
-			direction := game.BestMove()
-			//fmt.Printf("Moving %s\n", direction)
-			game.Swipe(direction)
-			score = game.BoardScore()
-			//fmt.Printf("Score is now %v\n", score)
-		}
-		numGames++
-		cumScore += score
+		playGame()
 	}
 	fmt.Printf("Avg score: %v\n", cumScore/float64(numGames))
+}
+
+func playGame() {
+	game := NewGame()
+	var score float64
+	for !game.IsOver {
+		//fmt.Println(game)
+		//fmt.Println()
+		direction := game.BestMove()
+		//fmt.Printf("Moving %s\n", direction)
+		game.Swipe(direction)
+		score = game.BoardScore()
+		//fmt.Printf("Score is now %v\n", score)
+	}
+	numGames++
+	cumScore += score
 }
